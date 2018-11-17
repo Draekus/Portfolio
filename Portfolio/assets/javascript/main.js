7a3b91e24363e62c8ddaeea409b76796ad48d04b
John Doe <patrick.gould94@gmail.com>
2018-11-17 12:03:34 -0500

    added

---
+++ Portfolio/assets/javascript/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+      });
+  });
+  
