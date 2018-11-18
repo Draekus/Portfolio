3d4f2625efc5a758b3fbe6db08e513ae9fa3761a
John Doe <patrick.gould94@gmail.com>
2018-11-17 20:50:35 -0500

    final changes made to firebase and contact page. Added confirmation modal for message sent.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,19 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCeV9Fh8IUqAp75Zeb6tI1GpSVIrii_7xs",
+    authDomain: "portfolio-e34d1.firebaseapp.com",
+    databaseURL: "https://portfolio-e34d1.firebaseio.com",
+    projectId: "portfolio-e34d1",
+    storageBucket: "",
+    messagingSenderId: "240680028322"
+  };
+  firebase.initializeApp(config);
+  
+  
+const database = firebase.database();
+
+
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.carousel').carousel();
@@ -8,7 +24,25 @@ $(document).ready(function(){
 
 $("#submit-button").on("click", function(){
     event.preventDefault();
-    
+    let name = $('#first_name').val().trim();
+    let email = $('#email').val().trim();
+    let message = $('#textarea2').val().trim();
+    let date = moment().format("MM-DD-YYYY");
+    console.log(name, email, message)
+    console.log(moment().format("MM-DD-YYYY"))
+    database.ref().push({
+
+        name: name,
+        email: email,
+        message: message,
+        date: date
+        
+        
+    });
+    $('#first_name').val('')
+    $('#email').val('')
+    $('#textarea2').val('')
+    $('.modal').modal();
 })
   });
   
